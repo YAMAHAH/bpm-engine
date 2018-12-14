65c871a09c02cb184c7a278c2bd19ebd76e9b54a
Linus Gubenis <linus.gubenis@camunda.com>
2018-12-14 16:57:43 +0100

    leu

--- packages/bpm-engine-persist-mongoose/src/index.js
+++ packages/bpm-engine-persist-mongoose/src/index.js
@@ -14,29 +14,29 @@ export default class MongoosePersist {
   processInstance = {
     create: obj => this.schemas.processInstance.create(obj),
     update: (query, patch) =>
-      this.schemas.processInstance.findOneAndUpdate(query, patch, { new: true }),
-    find: query => this.schemas.processInstance.findOne(query),
+      this.schemas.processInstance.findOneAndUpdate(query, patch, { new: true }).lean(),
+    find: query => this.schemas.processInstance.findOne(query).lean(),
   };
 
   tokenInstance = {
     create: obj => this.schemas.tokenInstance.create(obj),
     update: (query, patch) =>
-      this.schemas.tokenInstance.findOneAndUpdate(query, patch, { new: true }),
-    find: query => this.schemas.tokenInstance.findOne(query),
+      this.schemas.tokenInstance.findOneAndUpdate(query, patch, { new: true }).lean(),
+    find: query => this.schemas.tokenInstance.findOne(query).lean(),
   };
 
   workflowDefinition = {
     create: obj => this.schemas.workflowDefinition.create(obj),
-    find: query => this.schemas.workflowDefinition.findOne(query),
+    find: query => this.schemas.workflowDefinition.findOne(query).lean(),
     update: (query, patch) =>
-      this.schemas.workflowDefinition.findOneAndUpdate(query, patch, { new: true }),
+      this.schemas.workflowDefinition.findOneAndUpdate(query, patch, { new: true }).lean(),
   };
 
   timer = {
     create: obj => this.schemas.timer.create(obj),
     find: query => this.schemas.timer.findOne(query),
     update: (query, patch, options = {}) =>
-      this.schemas.timer.update(query, patch, { ...options, new: true }),
+      this.schemas.timer.update(query, patch, { ...options, new: true }).lean(),
     getNext: async (time) => {
       // get all timers where status is not 'done'
       // add a calculated field which is the time of creation of the timer and the time provided in this method as an argument
@@ -51,14 +51,14 @@ export default class MongoosePersist {
         { $limit: 1 },
       ];
 
-      const timer = await this.schemas.timer.aggregate(aggregation);
+      const timers = await this.schemas.timer.aggregate(aggregation);
 
-      return timer[0];
+      return timers[0];
     },
   };
 
   task = {
     create: obj => this.schemas.task.create(obj),
-    find: query => this.schemas.task.find(query),
+    find: query => this.schemas.task.find(query).lean(),
   };
 }
--- packages/bpm-engine-persist-mongoose/src/schema-initializer.js
+++ packages/bpm-engine-persist-mongoose/src/schema-initializer.js
@@ -4,19 +4,24 @@ import WorkflowDefinitionSchema from 'schemas/WorkflowDefinition';
 import TimerSchema from 'schemas/Timer';
 import TaskSchema from 'schemas/Task';
 
+const DEFAULTS = {
+  tokenInstance: 'bpmEngine_tokenInstances',
+  processInstance: 'bpmEngine_processInstances',
+  workflowDefinition: 'bpmEngine_workflowDefinitions',
+  timer: 'bpmEngine_timers',
+  task: 'bpmEngine_tasks',
+};
+
 export default (client, names = {}) => ({
-  tokenInstance: client.model(
-    names.tokenInstance || 'bpmEngine_tokenInstances',
-    TokenInstanceSchema,
-  ),
+  tokenInstance: client.model(names.tokenInstance || DEFAULTS.tokenInstance, TokenInstanceSchema),
   processInstance: client.model(
-    names.processInstance || 'bpmEngine_processInstances',
+    names.processInstance || DEFAULTS.processInstance,
     ProcessInstanceSchema,
   ),
   workflowDefinition: client.model(
-    names.workflowDefinition || 'bpmEngine_workflowDefinitions',
+    names.workflowDefinition || DEFAULTS.workflowDefinition,
     WorkflowDefinitionSchema,
   ),
-  timer: client.model(names.timer || 'bpmEngine_timers', TimerSchema),
-  task: client.model(names.task || 'bpmEngine_tasks', TaskSchema),
+  timer: client.model(names.timer || DEFAULTS.timer, TimerSchema),
+  task: client.model(names.task || DEFAULTS.task, TaskSchema),
 });
